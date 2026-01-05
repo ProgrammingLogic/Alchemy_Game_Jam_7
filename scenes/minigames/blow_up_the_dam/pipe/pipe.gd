09d3d0c04fc1defd67d25931df1b2c2dac755b46
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-01-04 19:35:41 -0600

    feature: add ability to spawn WaterParticle to Pipe

--- scenes/minigames/blow_up_the_dam/pipe/pipe.gd
+++ scenes/minigames/blow_up_the_dam/pipe/pipe.gd
@@ -1,22 +1,38 @@
 extends AnimatedSprite2D
+## Pipe that spawns water into the game.
+##
+## Initializes a timer with a 0.1 second delay and then connects the 
+## [method Node._create_water] to it's timeout signal. create_water will then
+## spawn a WaterParicle at $WaterSpawnPoint.
 
-@export var water_spawn_point: Node2D
-@onready var _water_created := false
+@export var is_spawning_water = true
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+@onready var water_spawn_timer: Timer = Timer.new()
+@onready var water_spawn_point: Node2D = $WaterSpawnPoint
+
+
+func _ready() -> void:	
+	water_spawn_timer.wait_time = 0.1
+	water_spawn_timer.autostart = true
 	
+	add_child(water_spawn_timer)
 	
-func _physics_process(delta: float) -> void:
-	if not _water_created:
-		create_water()
+	water_spawn_timer.start()
+	water_spawn_timer.timeout.connect(create_water)
+
+
+## Adds a water particle into the world.
+##
+## Creates a new WaterParticle object, with it's initial position equal to the
+## water_spawn_point's poistion. It then adds the WaterParticle to the root 
+## scene.
+func create_water() -> void:
+	if not is_spawning_water:
+		return
 		
-
-
-func create_water():
-	var _new_water_particle: WaterParticle = WaterParticle.new()
-	var _parent_scene = get_tree().get_root()
-	_parent_scene.add_child(_new_water_particle)
-	_new_water_particle.position = water_spawn_point.global_position
-	_water_created = true
+	var water_particle = WaterParticle.new()
+	var parent_scene: Node = get_tree().get_root()
+	
+	parent_scene.add_child(water_particle)
+	
+	water_particle.position = water_spawn_point.global_position
