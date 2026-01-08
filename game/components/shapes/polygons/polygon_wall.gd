af739cf60f5a0bf1fa8b6435c0cdf221d789285b
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-01-08 12:46:52 -0600

    feature: make polygon_wall inherit the properties of it's parent

--- game/components/shapes/polygons/polygon_wall.gd
+++ game/components/shapes/polygons/polygon_wall.gd
@@ -1,5 +1,5 @@
 class_name PolygonWall
-extends Polygon2D
+extends StaticBody2D
 ## Wall in the shape of a Polygon.
 ##
 ## Polygonal shape that acts as a "wall", preventing objects from moving through it.
@@ -13,33 +13,40 @@ var DEFAULT_COLLISION_MASKS: Dictionary = {
 }
 
 
-@onready var static_body: StaticBody2D
+@onready var parent_polygon_2d: Polygon2D
 @onready var collision_polygon: CollisionPolygon2D
 @onready var collision_layers: Dictionary
 @onready var collision_masks: Dictionary
+
 
 func _init(
 		collision_layers: Dictionary = DEFAULT_COLLISION_LAYERS, 
 		collision_masks: Dictionary = DEFAULT_COLLISION_MASKS
 ):
-	self.collision_layers = collision_layers
-	self.collision_masks = collision_masks
-
-
-func _ready() -> void:
-	if get_polygon().is_empty():
-		print("ERROR: Polygon has no verticies!")
-		return
-
-	static_body = StaticBody2D.new()
-	add_child(static_body)
-
-	collision_polygon = CollisionPolygon2D.new()
-	collision_polygon.set_polygon(get_polygon())
-	static_body.add_child(collision_polygon)
-
 	for collision_layer in collision_layers.keys():
-		static_body.set_collision_layer_value(
+		set_collision_layer_value(
 			int(collision_layer), 
 			collision_layers[collision_layer]
 		)
+
+	for collision_mask in collision_masks.keys():
+		set_collision_mask_value(
+			int(collision_mask), 
+			collision_layers[collision_mask]
+		)
+
+
+func _ready() -> void:
+	if not get_parent().get_class() == "Polygon2D":
+		print("ERROR: PolygonWall parent is not Polygon2D!")
+		return
+
+	parent_polygon_2d = get_parent()
+
+	if parent_polygon_2d.get_polygon().is_empty():
+		print("ERROR: Polygon has no verticies!")
+		return
+
+	collision_polygon = CollisionPolygon2D.new()
+	collision_polygon.set_polygon(parent_polygon_2d.get_polygon())
+	add_child(collision_polygon)
