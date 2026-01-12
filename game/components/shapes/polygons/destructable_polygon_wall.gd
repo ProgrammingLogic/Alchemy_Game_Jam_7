1f4da0bae58f3cf497bc15a1eaaeda0494f711c9
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-01-11 19:54:33 -0600

    feaure: add area2d to destructable wall

--- game/components/shapes/polygons/destructable_polygon_wall.gd
+++ game/components/shapes/polygons/destructable_polygon_wall.gd
@@ -7,6 +7,7 @@ extends Polygon2D
 
 @onready var static_body: StaticBody2D
 @onready var collision_polygon: CollisionPolygon2D
+@onready var area: Area2D
 
 
 func _ready() -> void:
@@ -19,10 +20,19 @@ func _ready() -> void:
 	static_body = _create_static_body(collision_polygon)
 	add_child(static_body)
 
+	area = _create_area(collision_polygon)
+
+
+func _create_collision_polygon() -> CollisionPolygon2D:
+	var c_polygon := CollisionPolygon2D.new()
+	c_polygon.set_polygon(get_polygon())
+
+	return c_polygon
+
 
 func _create_static_body(c_polygon: CollisionPolygon2D) -> StaticBody2D:
 	var s_body := StaticBody2D.new()
-	s_body.set_collision_layer_value(1, true) # Wall
+	s_body.set_collision_layer_value(1, false) # Wall
 	s_body.set_collision_mask_value(3, true) # Bomb
 	s_body.set_collision_mask_value(20, true) # Water
 
@@ -31,8 +41,8 @@ func _create_static_body(c_polygon: CollisionPolygon2D) -> StaticBody2D:
 	return s_body
 
 
-func _create_collision_polygon() -> CollisionPolygon2D:
-	var c_polygon := CollisionPolygon2D.new()
-	c_polygon.set_polygon(get_polygon())
+func _create_area(c_polygon: CollisionPolygon2D) -> Area2D:
+	var area := Area2D.new()
+	area.add_child(c_polygon)
 
-	return c_polygon
+	return area
