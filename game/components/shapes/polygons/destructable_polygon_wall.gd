93e4b51f59b14732c3c264190f85cd31a1323bf0
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-01-11 19:49:20 -0600

    feature: change polygon (again)

--- game/components/shapes/polygons/destructable_polygon_wall.gd
+++ game/components/shapes/polygons/destructable_polygon_wall.gd
@@ -1,49 +1,38 @@
 class_name DestructablePolygonWall
-extends Area2D
+extends Polygon2D
 ## Wall in the shape of a Polygon.
 ##
 ## Polygonal shape that acts as a "wall", preventing objects from moving through it.
 
 
 @onready var static_body: StaticBody2D
-@onready var parent_polygon_2d: Polygon2D
-@onready var polygon_2d: Polygon2D
 @onready var collision_polygon: CollisionPolygon2D
-@onready var collision_layers: Dictionary
-@onready var collision_masks: Dictionary
 
 
 func _ready() -> void:
-	if not get_parent().get_class() == "Polygon2D":
-		print("ERROR: DestructablePolygonWall parent is not Polygon2D!")
-		return
-
-	parent_polygon_2d = get_parent()
-
-	if parent_polygon_2d.get_polygon().is_empty():
+	if get_polygon().is_empty():
 		print("ERROR: DestructablePolygonWall parent has no verticies!")
 		return
 	
-	polygon_2d = Polygon2D.new()
-	polygon_2d.polygon = parent_polygon_2d.polygon
-	add_child(polygon_2d)
+	collision_polygon = _create_collision_polygon()
 
-	static_body = StaticBody2D.new()
-	static_body.set_collision_layer_value(1, true) # Wall
-	static_body.set_collision_mask_value(3, true) # Bomb
-	static_body.set_collision_mask_value(20, true) # Water
-	polygon_2d.add_child(static_body)
-	
-	collision_polygon = CollisionPolygon2D.new()
-	collision_polygon.set_polygon(parent_polygon_2d.get_polygon())
-	static_body.add_child(collision_polygon)
-
-	area_entered.connect(_on_area_entered)
-	body_entered.connect(_on_body_entered)
+	static_body = _create_static_body(collision_polygon)
+	add_child(static_body)
 
 
-func _on_area_entered():
-	print("area entered")
-	
-func _on_body_entered():
-	print("body entered")
+func _create_static_body(c_polygon: CollisionPolygon2D) -> StaticBody2D:
+	var s_body := StaticBody2D.new()
+	s_body.set_collision_layer_value(1, true) # Wall
+	s_body.set_collision_mask_value(3, true) # Bomb
+	s_body.set_collision_mask_value(20, true) # Water
+
+	s_body.add_child(c_polygon)
+
+	return s_body
+
+
+func _create_collision_polygon() -> CollisionPolygon2D:
+	var c_polygon := CollisionPolygon2D.new()
+	c_polygon.set_polygon(get_polygon())
+
+	return c_polygon
