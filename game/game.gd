d438c5d4ab1faa76230fbf0ca2b294659d89896d
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-01-14 14:40:36 -0600

    feature: add ability to draw debug polygon to game

--- game/game.gd
+++ game/game.gd
@@ -7,3 +7,18 @@ extends Node2D
 func _ready() -> void:
 	current_game = BlowUpTheDamGame.new(self)
 	add_child(current_game)
+
+func debug_draw_polygon(points: PackedVector2Array, duration := 10.0, color := Color.MAGENTA) -> void:
+	var debug_polygon := Polygon2D.new()
+	debug_polygon.color = color
+	debug_polygon.set_polygon(points)
+	debug_polygon.z_index = 500
+
+	var timer = Timer.new()
+	timer.timeout.connect(func():
+		debug_polygon.queue_free()
+	)
+
+	add_child(debug_polygon)
+	debug_polygon.add_child(timer)
+	timer.start(duration)
