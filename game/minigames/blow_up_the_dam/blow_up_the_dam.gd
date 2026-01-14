edd5ee4a2b294d5dfddf0f785566e16986d514ca
Jonathyn Stiverson <jlstiverson2002@protonmail.com>
2026-01-14 12:40:35 -0600

    feature: add basic ability to remove tiles

--- game/minigames/blow_up_the_dam/blow_up_the_dam.gd
+++ game/minigames/blow_up_the_dam/blow_up_the_dam.gd
@@ -2,21 +2,26 @@ class_name BlowUpTheDamGame
 extends Minigame
 
 
+@onready var tile_map: BlowUpTheDamTileMap
+
 
 func _ready():
-	pass
+	_setup_tile_map()
 
 
-func _input(event: InputEvent):
+func _setup_tile_map() -> void:
+	tile_map = BlowUpTheDamTileMap.new()
+	add_child(tile_map)
+
+
+func _input(event: InputEvent) -> void:
 	if event.is_action_released("place_bomb"):
 		_place_bomb()	
 
 
 func _place_bomb():
-	var m_pos: Vector2 = get_viewport().get_mouse_position()
-	var bomb := Bomb.new(m_pos)
-	#bomb.explosion_completed.conn?ect(_on_bomb_exploded)
-	add_child(bomb)
-
-func _test_polygon_2d_square_explosion_right_side() -> void:
-	pass 
+	var g_mouse: Vector2 = get_viewport().get_mouse_position()
+	var l_mouse: Vector2 = tile_map.to_local(g_mouse)
+	var cell: Vector2i = tile_map.local_to_map(l_mouse)
+	print("clearing cell (%d, %d)" % [cell.x, cell.y])
+	tile_map.erase_cell(cell)
